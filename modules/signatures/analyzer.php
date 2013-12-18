d99b1a60fd706dd5533cd53c6eb1197d0273d226
6Scan <6Scan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-18 13:08:16 +0000

    Improved security logging
    git-svn-id: https://plugins.svn.wordpress.org/6scan-protection/trunk@824665 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/signatures/analyzer.php
+++ modules/signatures/analyzer.php
@@ -1,8 +1,8 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) 
+if ( ! defined( 'ABSPATH' ) )
 	die( 'No direct access allowed' );
-		
+
 
 function sixscan_signatures_analyzer_is_env_flag_on( $flag_value ){
 
@@ -19,7 +19,7 @@ function sixscan_signatures_analyzer_requests_count( $is_suspicious = FALSE ){
 
 	if ( $is_suspicious )
 		$count_option_name = SIXSCAN_OPTION_STAT_SUSPICIOUS_REQ_COUNT;
-	
+
 	$counter_update_query = "update " . $wpdb->options . " set option_value=option_value+1 where option_name like '$count_option_name'";
 	$wpdb->query( $counter_update_query );
 }
@@ -27,7 +27,7 @@ function sixscan_signatures_analyzer_requests_count( $is_suspicious = FALSE ){
 function sixscan_signatures_analyzer_requests_reset(){
 	global $wpdb;
 
-	$counter_reset_query = "update " . $wpdb->options . " set option_value=0 where option_name like '" . SIXSCAN_OPTION_STAT_OK_REQ_COUNT . 
+	$counter_reset_query = "update " . $wpdb->options . " set option_value=0 where option_name like '" . SIXSCAN_OPTION_STAT_OK_REQ_COUNT .
 			"' or option_name like '" . SIXSCAN_OPTION_STAT_SUSPICIOUS_REQ_COUNT . "'";
 	$wpdb->query( $counter_reset_query );
 }
@@ -35,16 +35,16 @@ function sixscan_signatures_analyzer_requests_reset(){
 function sixscan_signatures_analyzer_requests_get(){
 	global $wpdb;
 
-	$counter_get_query = "select * from " . $wpdb->options . " where option_name like '" . SIXSCAN_OPTION_STAT_OK_REQ_COUNT . 
+	$counter_get_query = "select * from " . $wpdb->options . " where option_name like '" . SIXSCAN_OPTION_STAT_OK_REQ_COUNT .
 			"' or option_name like '" . SIXSCAN_OPTION_STAT_SUSPICIOUS_REQ_COUNT . "'";
-	
+
 	$analyzer_counter_array = $wpdb->get_results( $counter_get_query );
 
 	$response_array = array();
 
 	/* convert stdClass into associative array for ease of use */
 	foreach ( $analyzer_counter_array as $one_request ){
-		$response_array[ $one_request->option_name ] = $one_request->option_value;		
+		$response_array[ $one_request->option_name ] = $one_request->option_value;
 	}
 
 	return $response_array;
@@ -57,8 +57,8 @@ function sixscan_signatures_analyzer_deny_access(){
 	die();
 }
 
-function sixscan_signatures_analyzer_suspicious_request(){	
-	
+function sixscan_signatures_analyzer_suspicious_request(){
+
 	/* 	If we were accessed by one of our servers, do not count this request */
 	if ( strstr( SIXSCAN_SIGNATURE_SCANNER_IP_LIST, $_SERVER[ 'REMOTE_ADDR' ] ) !== FALSE )
 		return;
@@ -69,22 +69,39 @@ function sixscan_signatures_analyzer_suspicious_request(){
 		return;
 	}
 
+	list($analyze_action, $exploit_type) = sixscan_signatures_analyzer_is_to_block_request();
+	if ( $analyze_action == 'ignore' )
+		return;
+
 	/*	Suspicious request */
 	sixscan_signatures_analyzer_requests_count( TRUE );
-	
+
 	if ( is_writeable (dirname ( SIXSCAN_ANALYZER_LOG_FILEPATH ) . "/" ) == FALSE )
 		return;
-	
+
 	/* If it exists, we want to limit the filesize to some maximum */
 	if ( is_file( SIXSCAN_ANALYZER_LOG_FILEPATH ) && ( filesize( SIXSCAN_ANALYZER_LOG_FILEPATH  ) > SIXSCAN_ANALYZER_MAX_LOG_FILESIZE ) )
 		return;
-	
-	$data_log = "\"" . $_SERVER['SCRIPT_NAME'] . "\" \"" . addslashes( $_SERVER['QUERY_STRING'] ) . "\" \"" . addslashes( $_SERVER['HTTP_REFERER'] ) . "\" \"" . addslashes( $_SERVER['HTTP_USER_AGENT'] ) . "\"" . SIXSCAN_SECURITY_LOG_SEPARATOR;
+
+
+    /* get current request time in UTC */
+    date_default_timezone_set("UTC");
+    $current_time =  date("Y-m-d H:i:s", time());
+
+    /* we build a json string to provide easy parsing in the backend */
+    $data_log = json_encode(array(  'site' => $_SERVER['HTTP_HOST'],
+                                    'uri' => $_SERVER['REQUEST_URI'],
+                                    'query_string' => $_SERVER['QUERY_STRING'],
+                                    'script_name' => $_SERVER['SCRIPT_NAME'],
+                                    'exploit_type' => $exploit_type,
+                                    'remote_ip' => $_SERVER['REMOTE_ADDR'],
+                                    'remote_user_agent' => $_SERVER['HTTP_USER_AGENT'],
+                                    'remote_referrer' => $_SERVER['HTTP_REFERER'],
+                                    'time' => $current_time));
 
 	@file_put_contents( SIXSCAN_ANALYZER_LOG_FILEPATH , $data_log ,  FILE_APPEND );
 
-	/* Check whether we should block this request */
-	if ( sixscan_signatures_analyzer_is_to_block_request() )
+	if ( $analyze_action == 'block' )
 		sixscan_signatures_analyzer_deny_access();
 }
 
@@ -92,9 +109,9 @@ function sixscan_signatures_analyzer_is_rfi_by_mask( $requested_url, $required_m
 
 	$requested_url = strtolower( urldecode( $requested_url ) );
 	$required_mask = strtolower( $required_mask );
-	
+
 	$rfi_pattern = "/(https?|ftp|gzip|bzip2):\/\/([a-z0-9.-\/]+)&?/i";
-	
+
 	/* Get the URL, that address points to */
 	preg_match_all( $rfi_pattern, $requested_url, $rfi_matched );
 
@@ -106,7 +123,7 @@ function sixscan_signatures_analyzer_is_rfi_by_mask( $requested_url, $required_m
 	foreach ( $rfi_matched[2] as $one_rfi){
 		if ( $is_strict ){
 			if ( strcmp( $one_rfi, $required_mask ) != 0 )
-				return FALSE;	
+				return FALSE;
 		}
 		else{
 			if ( strncmp( $one_rfi, $required_mask, strlen( $required_mask ) ) != 0 )
@@ -118,69 +135,75 @@ function sixscan_signatures_analyzer_is_rfi_by_mask( $requested_url, $required_m
 }
 
 function sixscan_signatures_analyzer_is_to_block_request(){
-	$allowed_waf_rules = get_option( SIXSCAN_OPTION_WAF_REQUESTED );	
+	$allowed_waf_rules = get_option( SIXSCAN_OPTION_WAF_REQUESTED );
+
+	/*
+		Return values:
+		'ignore'  - do not block, do not log
+		'noblock' - log, don't block
+		'block' - log and block
+	*/
 
 	/* WAF is disabled */
-	if ( in_array( 'waf_global_enable' , $allowed_waf_rules ) == FALSE )		
-		return FALSE;
+	if ( in_array( 'waf_global_enable' , $allowed_waf_rules ) == FALSE )
+		return array('noblock','');
 
 	/* 	Filter strange requests */
 	if ( sixscan_signatures_analyzer_is_env_flag_on( "sixscanstrangerequest" ) ){
 		if ( in_array( 'waf_non_standard_req_disable' , $allowed_waf_rules ) )
-			return TRUE;
+			return array('block','');
 	}
 
 	/* 	Filter SQL injection */
 	if ( sixscan_signatures_analyzer_is_env_flag_on( "sixscanwafsqli" ) ){
 		if ( in_array( 'waf_sql_protection_enable' , $allowed_waf_rules ) )
-			return TRUE;
+            return array('block','sql');
 	}
 
 	/* 	Filter Cross Site Scripting */
 	if ( sixscan_signatures_analyzer_is_env_flag_on( "sixscanwafxss" ) ){
 		if ( in_array( 'waf_xss_protection_enable' , $allowed_waf_rules ) )
-			return TRUE;
+            return array('block','xss');
 	}
 
 	/* 	Filter CSRF on POST */
 	if ( sixscan_signatures_analyzer_is_env_flag_on( "sixscanwafcsrf" ) ){
 		if ( in_array( 'waf_post_csrf_protection_enable' , $allowed_waf_rules ) )
-			return TRUE;
-	}	
+            return array('block','csrf');
+	}
 
 	/* 	Filter RFI */
 	if ( sixscan_signatures_analyzer_is_env_flag_on( "sixscanwafrfi" ) ){
 		if ( in_array( 'waf_rfi_protection_enable' , $allowed_waf_rules ) ){
 			$allowed_rfi_scripts = array( '/wp-login.php' );
-			
+
 			/*	If link is OK to be used with URL as mask */
-			if ( in_array( $_SERVER['SCRIPT_NAME'] ,  $allowed_rfi_scripts ) )		
-				return FALSE;
+			if ( in_array( $_SERVER['SCRIPT_NAME'] ,  $allowed_rfi_scripts ) )
+				return array('ignore','');
 
 			/*	Allow local inclusions */
-			if ( in_array( 'waf_rfi_local_access_enable' , $allowed_waf_rules ) ){			
+			if ( in_array( 'waf_rfi_local_access_enable' , $allowed_waf_rules ) ){
 
 				$mixed_site_address = parse_url( home_url() );
-				
+
 				$current_hostname = $mixed_site_address[ 'host' ] ;
 
-				/* 	If the RFI doesn't satisfy requested mask - block the request. 
+				/* 	If the RFI doesn't satisfy requested mask - block the request.
 					Have to add "/", to avoid turning good domains (www.site.com) into bad (www.site.com.badsite.com) */
 				if ( ( sixscan_signatures_analyzer_is_rfi_by_mask( $_SERVER['QUERY_STRING'] , $current_hostname , TRUE ) == FALSE )
 					&&	( sixscan_signatures_analyzer_is_rfi_by_mask( $_SERVER['QUERY_STRING'] , $current_hostname . "/" ) == FALSE ) )
-					
-					return TRUE;
 
-				return FALSE;
+                    return array('block','rfi');
+
+                return array('ignore','');
 			}
 
 			/* RFI with no exclusions - always blocking */
-			return TRUE;
-		}		
-	}	
+            return array('block','rfi');
+		}
+	}
 
 	/* Trigger is not blocked */
-	return FALSE;
-
-}	
+    return array('noblock','');
+}
 ?>
